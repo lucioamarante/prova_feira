e9df8616fe27e43b43b053b40e90d1e1cb0b73c8
= <=>
2026-06-18 18:41:20 -0300

    ok

--- motorartigos/views.py
+++ motorartigos/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def index(request):
     #return HttpResponse('<h1>Oi</h1>')
     return render(request,'motorartigos/index.html')
+
+def artigo(request):
+    return render(request,'motorartigos/artigo.html')
