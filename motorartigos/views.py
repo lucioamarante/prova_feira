444b594c3ae8955876933b0d27e6d3f26045d608
= <=>
2026-06-19 16:15:06 -0300

    k

--- motorartigos/views.py
+++ motorartigos/views.py
@@ -2,7 +2,25 @@ from django.shortcuts import render
 #from django.http import HttpResponse
 def index(request):
     #return HttpResponse('<h1>Oi</h1>')
-    return render(request,'motorartigos/index.html')
+    # estrutura de dados em Python
+    #lista = [1,2,3,4,5,6]  #Mutável
+    #tupla = (1,2,3,4,5,6)  #Imutável
+    #combo_mac = ('big mac','coca cola','batatinha')
+    PI_MAT = 3.14
+    # chave e valor DICIONÁRIO
+    autores = {
+        1:{"nome":"André Roglem",
+           "biografia":"estudante do SENAI de BD",
+           "email":"roglem@nasa.gov.br"           
+          },
+        2:{"nome":"Luiz Fernando",
+           "biografia":"Desenvolvedor Django ",
+           "email":"fernando@gmail.com"},
+        3:{"nome":"Victor Jonh",
+           "biografia":"Desenvolvedor SQL ",
+           "email":"victor@gmail.com"}
+    }
+    return render(request,'motorartigos/index.html',{"autores":autores})
 
 def artigo(request):
     return render(request,'motorartigos/artigo.html')
