b209db6b17b37caa9af3ce8752b06298fd9f85d8
= <=>
2026-06-19 17:25:20 -0300

    ok

--- motorartigos/views.py
+++ motorartigos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 #from django.http import HttpResponse
+from motorartigos.models import Autor
 def index(request):
     #return HttpResponse('<h1>Oi</h1>')
     # estrutura de dados em Python
@@ -8,7 +9,9 @@ def index(request):
     #combo_mac = ('big mac','coca cola','batatinha')
     PI_MAT = 3.14
     # chave e valor DICIONÁRIO
-    autores = {
+    # MOCK OBJECTS
+    """
+       autores = {
         1:{"nome":"André Roglem",
            "biografia":"estudante do SENAI de BD",
            "email":"roglem@nasa.gov.br"           
@@ -20,6 +23,10 @@ def index(request):
            "biografia":"Desenvolvedor SQL ",
            "email":"victor@gmail.com"}
     }
+    
+    """
+    autores = Autor.objects.all()
+   
     return render(request,'motorartigos/index.html',{"autores":autores})
 
 def artigo(request):
